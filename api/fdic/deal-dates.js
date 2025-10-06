9132da943bd510b6f550c462ffcb3576200e389c
bmhoffman07 <brandonhoffman07@gmail.com>
2025-10-05 21:34:55 -0400

    Delete api/fdic/deal-dates.js

--- api/fdic/deal-dates.js
+++
@@ -1,9 +0,0 @@
-export const config = { runtime: 'edge' };
-export default async function handler() {
-  return new Response('null', {
-    headers: {
-      'content-type': 'application/json; charset=utf-8',
-      'access-control-allow-origin': '*',
-    },
-  });
-}
